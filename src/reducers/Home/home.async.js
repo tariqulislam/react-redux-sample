83572c0b3c817c33fc52e31fda48450b303a388f
tariqul Islam <tariqul.islam.rony@gmail.com>
2020-02-08 02:28:24 +0900

    add the weather calculation

--- src/reducers/Home/home.async.js
+++ src/reducers/Home/home.async.js
@@ -11,3 +11,20 @@ export const getAllPosts = async () => {
    )
    return getAllPosts;
 }
+
+export const getAllWeatherInfo = () => {
+   return new Promise((resolve, reject) => {
+      axios.get(
+         "https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=b6907d289e10d714a6e88b30761fae22")
+         .then(res => {
+            debugger
+            if (res.status === 200) {
+               resolve(res)
+            } else {
+               reject(res)
+            }
+         });
+   })
+  
+  
+}
--- src/reducers/Home/home.reducer.js
+++ src/reducers/Home/home.reducer.js
@@ -1,54 +1,90 @@
-import {getAllPosts}   from './home.async'
+import {getAllWeatherInfo}   from './home.async'
 export const HIDE_HOME_INFO = 'HIDE_HOME_INFO'
 export const SHOW_HOME_INFO = 'SHOW_HOME_INFO'
 export const GET_ALL_POSTS = 'GET_ALL_POSTS'
 
+export const GET_ALL_WEATHER_INFO = 'GET_ALL_WEATHER_INFO'
 
-export const showParagraphInfo = () => {
-    return dispatch => { dispatch({  type: SHOW_HOME_INFO })}
-}
 
-export const hideParagraphInfo = () => {
-    return dispatch => { dispatch({  type: HIDE_HOME_INFO })}
-}
 
-export const getAllPostForFromApi = () => {
+export const getAllWeatherInfoFromApi = () => {
     return dispatch => {
-         getAllPosts().then(res => {
+    
+        getAllWeatherInfo().then(res => {
+            debugger
+            /** calculating the weather data */
+            let weatherInfo = res.data && res.data.list;
+            let weatherTypes = ["Clouds", "Clear", "Rain"]
+            /** get the dateinfo */
+            let dates = [];
+            let newWeatherInfo = [];
+            weatherInfo.forEach(val => {
+                let tempDate = val.dt_txt.split(' ')
+                dates = [...dates, tempDate[0]]
+                let main = val.weather && val.weather[0].main;
+                let tempWeatherInfo = { date: tempDate[0], timetext: tempDate[1], weather: main }
+                newWeatherInfo = [...newWeatherInfo, tempWeatherInfo]
+            })
+            
+            let uniqueDate = dates.filter((elem,pos) => dates.indexOf(elem) === pos)
+
+            let consolidateInfo = {};
+
+            uniqueDate.forEach(elem => {
+                let key = `${elem}`
+                let tempClouds = 0;
+                let tempRain =0;
+                let tempClear =0;
+                let total = 0
+                let tempWeather = [];
+                newWeatherInfo.forEach(mElem => {
+                    if(elem === mElem.date) {
+                       if(mElem.weather == 'Clear') {
+                           tempClear = tempClear + 1;
+                       }
+
+                       if(mElem.weather == 'Clouds') {
+                           tempClouds = tempClouds + 1;
+                       }
+
+                       if(mElem.weather == 'Rain') {
+                           tempRain = tempRain + 1;
+                       }
+                       total = total + 1;
+                       tempWeather = [...tempWeather, mElem]
+                    }
+                })
+                consolidateInfo[key] = {"clouds": tempClouds,
+                 "rain": tempRain,
+                  "clear": tempClear,
+                  "total": total,
+                   "weather": tempWeather}
+            })
+
+            debugger
             dispatch({
-                type: GET_ALL_POSTS,
-                payload: {...res.data}
+                type: GET_ALL_WEATHER_INFO,
+                payload: {
+                    ...res.data
+                }
             })
         })
-      
     }
 }
 
 
-
 const initialState = {
-    showInfo: false,
-    posts: null
+    weathers: [],
 }
 
 
 export default (state= initialState, action) => {
     switch (action.type) {
-        case SHOW_HOME_INFO:
+        case GET_ALL_WEATHER_INFO:
             return {
                 ...state,
-                showInfo: true
+                weathers: action.payload
             }
-        case HIDE_HOME_INFO:
-            return {
-                ...state,
-                showInfo: false
-            }
-        case GET_ALL_POSTS:
-            return {
-                ...state,
-                posts: action.payload
-            }    
         default:
             return state
     }
