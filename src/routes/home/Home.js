d1ec7575b11905a3a9e7104683ae5f7ce0407062
tariqul Islam <tariqul.islam.rony@gmail.com>
2020-03-23 13:12:06 +0900

    fix the work

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -9,7 +9,7 @@ import {
 } from '../../reducers/Home/home.reducer'
 import { Row, Col, Image } from 'react-bootstrap'
 import GroupOnDevice from './assets/images/group-on-devices.jpg'
-
+import {withTranslation} from 'react-i18next'
 
 
 class Home extends Component {
@@ -17,6 +17,7 @@ class Home extends Component {
        let bannerStyle = {
            width: "100%"
        }
+       const {t} = this.props
        return (  
         <>
          <Row>
@@ -26,9 +27,30 @@ class Home extends Component {
          </Row>
          <Row>
              <Col xs={12}>
-                
+                <Col xs={4}>
+                </Col>
+                <Col xs={4}>
+                    <h1>{t('homepage.initial_section.whatsGHRN')}</h1>
+                </Col>
+                <Col xs={4}>
+                </Col>
              </Col>
+             <Col xs={12}>
+                 <Col xs={2}></Col>
+                 <Col xs={4}>
+                    <Col xs={6}>
+                      {t('')}
+                    </Col>
+                    <Col xs={6}>
+
+                    </Col>
+
+                 </Col>
+                 <Col xs={2}></Col>
+             </Col>
+            
          </Row>
+
         </>
        
        )
@@ -47,7 +69,7 @@ const mapDispatchToProps = dispatch => bindActionCreators({
     goToAboutPage: () => push('/about-us')
 }, dispatch)
 
-export default connect(
+export default withTranslation()(connect(
     mapStateToProps,
     mapDispatchToProps
-)(Home)
+)(Home))
