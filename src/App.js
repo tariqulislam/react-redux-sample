c889604b89f404ce86287e892b1ad04596dc9430
Aeon Tanvir <aeontanvir@gmail.com>
2020-04-16 23:30:13 +0600

    Campaign page added

--- src/App.js
+++ src/App.js
@@ -1,46 +1,48 @@
 import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
 import Home from "./routes/home/Home";
+import Campaign from "./routes/campaign/Campaign";
 import CandidateRegister from "./routes/candidate/register/Register";
-import CompanyRegister from './routes/company/register/Register';
-import AdminLogin from './routes/admin/login/Login';
+import CompanyRegister from "./routes/company/register/Register";
+import AdminLogin from "./routes/admin/login/Login";
 import "./assets/App.css";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import CandidateLogin from "./routes/candidate/Login/Login";
-import AdminDashboard from './routes/admin/admin'
-import { withTranslation } from 'react-i18next'
-import Campaign from './routes/admin/campain/components/campaign'
+import AdminDashboard from "./routes/admin/admin";
+import { withTranslation } from "react-i18next";
+import CampaignAdmin from "./routes/admin/campain/components/campaign";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      value: "en"
-    }
-  }
- 
-  onLanguageHandle = (event) => {
-    this.setState({value: event})
-    localStorage.setItem('language', event);
-    this.props.i18n.changeLanguage(event)
+      value: "en",
+    };
   }
 
+  onLanguageHandle = (event) => {
+    this.setState({ value: event });
+    localStorage.setItem("language", event);
+    this.props.i18n.changeLanguage(event);
+  };
+
   componentDidMount() {
-    let getLanguageVal = localStorage.getItem('language')
-    this.setState({value: getLanguageVal})
-    this.props.i18n.changeLanguage(getLanguageVal)
+    let getLanguageVal = localStorage.getItem("language");
+    this.setState({ value: getLanguageVal });
+    this.props.i18n.changeLanguage(getLanguageVal);
   }
 
   render() {
-
     return (
-       <React.Fragment>
+      <React.Fragment>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Navbar.Brand href="javascript.void(0);"><Link to="/">GHRN</Link></Navbar.Brand>
+          <Navbar.Brand href="javascript.void(0);">
+            <Link to="/">GHRN</Link>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="/campaign">Campaign</Nav.Link>
               <Nav.Link href="#pricing">Pricing</Nav.Link>
               <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -57,31 +59,49 @@ class App extends Component {
               </NavDropdown>
             </Nav>
             <Nav>
-              
-              <Nav.Link ><Link to="/candidate/register">Register</Link></Nav.Link>
-              <Nav.Item style={{color: 'white', marginTop:'8px', cursor: 'pointer'}} eventKey={2} onClick={() => this.onLanguageHandle('en')}>EN </Nav.Item>
-              <Nav.Item style={{color: 'white', marginTop: '8px', cursor: 'pointer'}} eventKey={2} onClick={() => this.onLanguageHandle('jp')}>&nbsp;|&nbsp;JP</Nav.Item>
+              <Nav.Link>
+                <Link to="/candidate/register">Register</Link>
+              </Nav.Link>
+              <Nav.Item
+                style={{ color: "white", marginTop: "8px", cursor: "pointer" }}
+                eventKey={2}
+                onClick={() => this.onLanguageHandle("en")}
+              >
+                EN{" "}
+              </Nav.Item>
+              <Nav.Item
+                style={{ color: "white", marginTop: "8px", cursor: "pointer" }}
+                eventKey={2}
+                onClick={() => this.onLanguageHandle("jp")}
+              >
+                &nbsp;|&nbsp;JP
+              </Nav.Item>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-       
+
         {/* <header className="App-header">
           <Link to="/"> Home</Link>
           <Link to="/about-us"> About </Link>
         </header> */}
 
-         <main>
-          <Route exact path="/" component={Home}  />
-          <Route exact path="/admin/campaign" component={Campaign} />
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/campaign" component={Campaign} />
+          <Route exact path="/admin/campaign" component={CampaignAdmin} />
           <Route exact path="/admin/login" component={AdminLogin} />
-          <Route exact path="/candidate/register" component={CandidateRegister}  />
+          <Route
+            exact
+            path="/candidate/register"
+            component={CandidateRegister}
+          />
           <Route exact path="/company/register" component={CompanyRegister} />
-          <Route exact path="/candidate/login" component={CandidateLogin}  />
+          <Route exact path="/candidate/login" component={CandidateLogin} />
           <Route exact path="/admin/dashboard" component={AdminDashboard} />
         </main>
-        </React.Fragment>
+      </React.Fragment>
     );
   }
 }
 
-export default withTranslation()(App)
+export default withTranslation()(App);
